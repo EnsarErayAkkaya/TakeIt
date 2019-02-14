5a03c4379beb4e954635452ef1a2d63f1e6019a2
Ensar Eray Akkaya <ensar.eray.1994@gmail.com>
2019-02-14 09:46:22 +0300

    Inıitial
    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TakeIt.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
